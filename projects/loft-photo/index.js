f23cd97538c24750b1371256cb297bf62c02ad0a
Aram152 <nn.aram@yandex.ru>
2023-07-02 15:23:22 +0300

    week-4_hwk

--- projects/loft-photo/index.js
+++ projects/loft-photo/index.js
@@ -3,7 +3,14 @@ import('./styles.css');
 
 const pageNames = ['login', 'main', 'profile'];
 
-document.addEventListener('click', () => {
-    const pageName = model.getRandomElement(pageNames);
-    pages.openPage(pageName);
-});
+// document.addEventListener('click', () => {
+//     const pageName = model.getRandomElement(pageNames);
+//     pages.openPage(pageName);
+// });
+
+import mainPage from './mainPage';
+import loginPage from './loginPage';
+
+pages.openPage('login');
+loginPage.handleEvents();
+mainPage.handleEvents();
